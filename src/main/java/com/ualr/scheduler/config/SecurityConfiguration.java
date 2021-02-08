fbf4adc94bd304602ff043b6d5f09ecc661774d1
rgabhawala <rgabhawala@users.noreply.github.com>
2021-02-07 23:53:05 -0600

    adding login check

--- src/main/java/com/ualr/scheduler/config/SecurityConfiguration.java
+++ src/main/java/com/ualr/scheduler/config/SecurityConfiguration.java
@@ -21,7 +21,7 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 @EnableWebSecurity
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
-    @Qualifier("userDetailsServiceImpl")
+    @Qualifier("inMemoryUserDetailsManager")
     @Autowired
     private UserDetailsService userDetailsService;
 
