97e806ef0f9349af827b8540254d5fc9db31c377
rgabhawala <rgabhawala@users.noreply.github.com>
2021-02-08 00:03:59 -0600

    adding login check

--- src/main/java/com/ualr/scheduler/config/SecurityConfiguration.java
+++ src/main/java/com/ualr/scheduler/config/SecurityConfiguration.java
@@ -28,7 +28,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
     @Autowired
     public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
-        auth.userDetailsService(userDetailsService).passwordEncoder(passwordEncoder());
+        auth.userDetailsService(userDetailsService);
     }
 
     @Override
